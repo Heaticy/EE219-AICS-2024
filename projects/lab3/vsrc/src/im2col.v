a2515c7dd77b899c88e3c62a2ae6b0632a1211c8
AICS-ShanghaiTech <tracer@ics>
2024-11-22 16:45:15 +0800

    Heaticy 2021531045    Static hostname: 2024-znjsxt18          Icon name: computer-vm            Chassis: vm         Machine ID: d20345ef49df4cdb803e392c7c2fb3aa            Boot ID: 6beca36615cc4f52a273672fc97a1db3     Virtualization: vmware   Operating System: Ubuntu 20.04.6 LTS             Kernel: Linux 5.13.0-30-generic       Architecture: x86-64  16:45:16 up 8 days,  1:00,  0 users,  load average: 0.00, 0.00, 0.00

--- projects/lab3/vsrc/src/im2col.v
+++ projects/lab3/vsrc/src/im2col.v
@@ -145,45 +145,43 @@ always@(posedge clk or negedge rst_n)begin
                 done <= 0;
                 mem_wr_en <= 1;
                 addr_wr <= IM2COL_BASE;
-                x <= DATA_WIDTH - 1;
                 count <= 1;
-                data_wr[DATA_WIDTH-1:0] <= IMG_PADDING_BUFFER[x -: DATA_WIDTH];
+                data_wr[DATA_WIDTH-1:0] <= IMG_PADDING_BUFFER[DATA_WIDTH-1:0];
             end
             else begin
                 addr_wr <= addr_wr + 1;
-                if(filter_col == FILTER_SIZE - 1)begin
-                    filter_col <= 0;
-                    if(filter_row == FILTER_SIZE - 1)begin
-                        filter_row <= 0;
-                        if(channel == IMG_C - 1)begin
-                            channel <= 0;
-                            if(col == IMG_W - 1)begin
-                                col <= 0;
-                                if(row == IMG_H - 1)begin
-                                    row <= 0;
-                                end
-                                else begin
-                                    row <= row + 1;
-                                end
+            end
+            if(filter_col == FILTER_SIZE - 1)begin
+                filter_col <= 0;
+                if(filter_row == FILTER_SIZE - 1)begin
+                    filter_row <= 0;
+                    if(channel == IMG_C - 1)begin
+                        channel <= 0;
+                        if(col == IMG_W - 1)begin
+                            col <= 0;
+                            if(row == IMG_H - 1)begin
+                                row <= 0;
                             end
                             else begin
-                                col <= col + 1;
+                                row <= row + 1;
                             end
                         end
                         else begin
-                            channel <= channel + 1;
+                            col <= col + 1;
                         end
                     end
                     else begin
-                        filter_row <= filter_row + 1;
+                        channel <= channel + 1;
                     end
                 end
                 else begin
-                    filter_col <= filter_col + 1;
+                    filter_row <= filter_row + 1;
                 end
-                x<=(channel * PADDING_H * PADDING_W + (row + filter_row) * PADDING_W + col + filter_col + 1) * DATA_WIDTH - 1;
-                data_wr[DATA_WIDTH-1:0] <= IMG_PADDING_BUFFER[x -: DATA_WIDTH];
             end
+            else begin
+                filter_col <= filter_col + 1;
+            end
+            data_wr[DATA_WIDTH-1:0] <= IMG_PADDING_BUFFER[(channel * PADDING_H * PADDING_W + (row + filter_row) * PADDING_W + col + filter_col + 1) * DATA_WIDTH - 1 -: DATA_WIDTH];
         end
         DONE: begin
             done <= 1;
