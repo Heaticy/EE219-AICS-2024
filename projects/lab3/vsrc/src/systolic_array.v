87e698aa41f1ad0e632f9bf01f5596bdb6830136
AICS-ShanghaiTech <tracer@ics>
2024-11-24 01:57:10 +0800

    Heaticy 2021531045    Static hostname: 2024-znjsxt18          Icon name: computer-vm            Chassis: vm         Machine ID: d20345ef49df4cdb803e392c7c2fb3aa            Boot ID: 6beca36615cc4f52a273672fc97a1db3     Virtualization: vmware   Operating System: Ubuntu 20.04.6 LTS             Kernel: Linux 5.13.0-30-generic       Architecture: x86-64  01:57:10 up 9 days, 10:12,  0 users,  load average: 0.05, 0.01, 0.00

--- projects/lab3/vsrc/src/systolic_array.v
+++ projects/lab3/vsrc/src/systolic_array.v
@@ -2,6 +2,35 @@
 
 `timescale 1ns / 1ps
 
+module FIFO #(
+    parameter DATA_WIDTH = 32,
+    parameter FIFO_LEN = 8
+) (
+    input clk,
+    input rst_n,
+    input [DATA_WIDTH-1:0] data_in,
+    output [DATA_WIDTH-1:0] data_out
+);
+
+reg [DATA_WIDTH-1:0] fifo [0:FIFO_LEN-1];
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        for (integer i = 0; i < FIFO_LEN; i = i + 1) begin
+            fifo[i] <= 0;
+        end
+    end else begin
+        fifo[0] <= data_in;
+        for (integer i = 1; i < FIFO_LEN; i = i + 1) begin
+            fifo[i] <= fifo[i-1];
+        end
+    end
+end
+
+assign data_out = fifo[FIFO_LEN-1];
+
+endmodule
+
 module systolic_array#(
     parameter M           = 5,
     parameter N           = 3,
@@ -23,8 +52,35 @@ wire [DATA_WIDTH-1:0] x_wire [0:M-1][0:K-1];
 wire [DATA_WIDTH-1:0] w_wire [0:M-1][0:K-1];
 wire [DATA_WIDTH-1:0] y_wire [0:M-1][0:K-1];
 
-// Instantiate the systolic array using generate
 genvar i, j;
+generate
+    for (i = 1; i < M; i = i + 1) begin : row
+        FIFO #(
+            .DATA_WIDTH(DATA_WIDTH),
+            .FIFO_LEN(i)
+        ) fifo_inst (
+            .clk(clk),
+            .rst_n(rst_n),
+            .data_in( X[DATA_WIDTH*(i+1)-1:DATA_WIDTH*i]),
+            .data_out(x_wire[i][0])
+        );
+    end
+    for (i = 1; i < K; i = i + 1) begin : col
+        FIFO #(
+            .DATA_WIDTH(DATA_WIDTH),
+            .FIFO_LEN(i)
+        ) fifo_inst (
+            .clk(clk),
+            .rst_n(rst_n),
+            .data_in( W[DATA_WIDTH*(i+1)-1:DATA_WIDTH*i]),
+            .data_out(w_wire[0][i])
+        );
+    end
+endgenerate
+
+
+// Instantiate the systolic array using generate
+
 generate
     for (i = 0; i < M; i = i + 1) begin : row
         for (j = 0; j < K; j = j + 1) begin : col
@@ -34,9 +90,9 @@ generate
                 .clk(clk),
                 .rst(rst_n),
                 // Input X: the first column gets input from the external X signal, others get from previous column
-                .x_in((j == 0) ? X[DATA_WIDTH*(i+1)-1:DATA_WIDTH*i] : x_wire[i][j-1]),
+                .x_in((i == 0) ? X[DATA_WIDTH - 1:0] : x_wire[i][j]),
                 // Input W: the first row gets input from the external W signal, others get from the previous row
-                .w_in((i == 0) ? W[DATA_WIDTH*(j+1)-1:DATA_WIDTH*j] : w_wire[i-1][j]),
+                .w_in((j == 0) ? W[DATA_WIDTH - 1:0] : w_wire[i][j]),
                 // Output signals to be passed to the next PE
                 .x_out(x_wire[i][j]),
                 .w_out(w_wire[i][j]),
