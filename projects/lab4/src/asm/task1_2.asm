38cd7838d5391d6fdc001b7efc4a538c47cb9b29
Heaticy <1712371381@qq.com>
2024-12-21 16:42:32 +0800

    1-2finishing task1_2

--- projects/lab4/src/asm/task1_2.asm
+++ projects/lab4/src/asm/task1_2.asm
@@ -1,52 +1,37 @@
-; =======================================
-; task1_2
-; =======================================
 main:
-    addi    x9,x0,0
-    jal     x0,.L2
-.L7:
-    addi    x18,x0,0
-    jal     x0,.L3
-.L6:
-    addi    x19,x0,0
-    slli    x15,x9,3
-    add     x15,x18,x15
-    slli    x15,x15,2
-    lui     x13,2152726528 ;c 
-    add     x13,x13,x15
-    lw      x12,0(x13)
-    jal     x0,.L4
-.L5:
-    slli    x15,x9,3
-    add     x15,x19,x15
-    slli    x15,x15,2
-    lui     x14 2148532224  ;a
-    add     x15,x14,x15
-    lw      x14,0(x15)
-    slli    x15,x18,3
-    add     x15,x19,x15
-    slli    x15,x15,2
-    lui     x13 2151677952  ;bt
-    add     x15,x13,x15
-    lw      x15,0(x15)
-    mul     x14,x14,x15
-    add     x12,x12,x14
-    addi    x19,x19,1
-.L4:
-    addi    x15,x0,8
-    blt     x19,x15,.L5
-    slli    x15,x9,3
-    add     x15,x18,x15
-    slli    x15,x15,2
-    lui     x13,2154823680 ;d
-    add     x13,x13,x15
-    sw      x12,0(x13)
-    addi    x18,x18,1 
-.L3:
-    addi    x15 x0 8
-    blt     x18,x15,.L6
-    addi    x9,x9,1
+        lui     x29,2148532224
+        lui     x5,2152726528
+        lui     x31,2154823680
+        lui     x8,2151677952
+        addi    x10 x29 32
+        addi    x7 x0 0
+        addi    x30 x0 64
 .L2:
-    addi    x15 x0 8
-    blt     x9,x15,.L7
-halt
+        addi    x28 x8 0
+        addi    x6 x31 0
+        addi    x17 x5 0
+        addi    x16 x0 0
+.L4:
+        lw      x12,0(x17)
+        addi    x13 x28 0
+        addi    x15 x29 0
+.L3:
+        lw      x14,0(x15)
+        lw      x11,0(x13)
+        addi    x15,x15,4
+        addi    x13,x13,4
+        mul     x14,x14,x11
+        add     x12,x12,x14
+        bne     x15,x10,.L3
+        sw      x12,0(x6)
+        addi    x16,x16,8
+        addi    x17,x17,4
+        addi    x6,x6,4
+        addi    x28,x28,32
+        bne     x16,x30,.L4
+        addi    x7,x7,8
+        addi    x29,x29,32
+        addi    x10,x15,32
+        addi    x5,x5,32
+        addi    x31,x31,32
+        bne     x7,x16,.L2
